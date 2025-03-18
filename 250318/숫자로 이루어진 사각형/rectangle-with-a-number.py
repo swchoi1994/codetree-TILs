faf53d437cb91cede5a85344523c9d49d40c9068
Seongwoo Choi <swchoi1994@gmail.com>
2025-03-18 14:01:01 +0900

    Title: 숫자로 이루어진 사각형, Time: 47ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250318/숫자로 이루어진 사각형/rectangle-with-a-number.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+# Please write your code here.
+def printNum(n):
+    num = 1
+    for i in range(n):
+        row = ''
+        for j in range(n):
+            row += str(num) + ' '
+            num += 1
+            if num > 9:
+                num = 1
+        print(row.strip())
+
+printNum(4)
